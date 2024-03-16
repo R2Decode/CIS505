cd6c6ebd0c49a5b47c01db642158c0e38d33049c
Sean <seturner@my365.bellevue.edu>
2024-03-16 19:00:06 -0400

    Add TurnerHelloWorld program

---
+++ TurnerHelloWorld.java
@@ -0,0 +1,8 @@
+package CIS505;
+public class TurnerHelloWorld {
+    public static void main(String[] args) {
+        String firstName = "Sean"; 
+        String lastName = "Turner"; 
+        System.out.printf("Hello World from %s %s.\n", firstName, lastName);
+    }
+}
